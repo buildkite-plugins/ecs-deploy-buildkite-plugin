ad59feb0ac34c8acabd14deb96ccec127383cb65
Matías A. Bellone <matias.bellone@buildkite.com>
2022-10-29 11:46:12 -0300

    Some more refactoring

--- lib/buildkite.bash
+++ lib/buildkite.bash
@@ -26,3 +26,30 @@ function plugin_read_list() {
   prefix_read_list "BUILDKITE_PLUGIN_${PLUGIN_PREFIX}_${1}"
 }
 
+
+# Reads either a value or a list from plugin config into a global result array
+# Returns success if values were read
+function prefix_read_list_into_result() {
+  local prefix="$1"
+  local parameter="${prefix}_0"
+  result=()
+
+  if [[ -n "${!parameter:-}" ]]; then
+    local i=0
+    local parameter="${prefix}_${i}"
+    while [[ -n "${!parameter:-}" ]]; do
+      result+=("${!parameter}")
+      i=$((i+1))
+      parameter="${prefix}_${i}"
+    done
+  elif [[ -n "${!prefix:-}" ]]; then
+    result+=("${!prefix}")
+  fi
+
+  [[ ${#result[@]} -gt 0 ]] || return 1
+}
+
+# Reads either a value or a list from plugin config
+function plugin_read_list_into_result() {
+  prefix_read_list_into_result "BUILDKITE_PLUGIN_${PLUGIN_PREFIX}_${1}"
+}
